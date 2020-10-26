f756918143434443a3d361d8ba41b17e0f9b7517
sydney miranda <sydmiranda@icloud.com>
2020-10-26 21:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+
+}
+
+destructivelyRemoveFirstKitten() {
+
+}
+
+function appendKitten(name) {
+
+}
+
+function prependKitten(name) {
+
+}
+
+function removeLastKitten() {
+
+}
+
+function removeFirstKitten() {
+
+}
